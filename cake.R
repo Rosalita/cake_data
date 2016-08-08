aeb510eb9fd855f90450a215a075e3457af485e0
Rosie <enchantrix@gmail.com>
2016-08-08 22:04:29 +0100

    first basic plot

---
+++ cake.R
@@ -0,0 +1,15 @@
+#install.packages("readxl")
+library(readxl)
+library(ggplot2)
+
+setwd("C:/git/cake_data")
+data <- read_excel("cake_data.xlsx")
+
+# clean NA rows out
+data <- na.omit(data)
+
+ggplot(data, aes(Date, Time, colour = Food))+
+geom_point() +
+ggtitle("Free Food Left in the Kitchen at Work")+
+labs(x="Date", y="Time of Day")
+
